080457fe3aacf42623cca9625e0c0bc8a743f0b4
Mohsin Khan <mohsin.khan1@autodesk.com>
2023-11-13 00:20:51 +0530

    added app  bar and image assets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Breakfast",
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: GestureDetector(
+          onTap: () => {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+            child: SvgPicture.asset(
+              "assets/icons/back_nav.svg",
+              height: 25,
+              width: 25,
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () => {},
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(10),
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: SvgPicture.asset(
+                "assets/icons/dots.svg",
+                height: 25,
+                width: 25,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
